881a590855c687681786cc2c52d450a13db8b8d4
DemonDaddy22 <guptarohan369@gmail.com>
2019-07-31 12:12:12 +0530

    save fetched image to pwd

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 import os
+import ctypes
 import requests
+import urllib.request
 
 key = os.environ.get("UNSPLASH_ACCESS_KEY")
 url = "https://api.unsplash.com/photos/random/?client_id=" + key
 response = requests.get(url)
 data = response.json()
+img = data["urls"]["full"]
+name = data["alt_description"].replace(" ", "-")
+urllib.request.urlretrieve(img, name + '.jpg')
 # print(data["urls"])
